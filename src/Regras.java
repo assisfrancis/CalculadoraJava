1a96aacaafd5f383248574d513ffd4321da7a061
Francisco A. silva <assis.francis123@hotmail.com>
2024-05-23 22:49:41 -0300

    Terceiro commit

---
+++ src/Regras.java
@@ -0,0 +1,7 @@
+public interface Regras {
+    
+    public double somar(double num1,double num2);    
+    public double subtrair(double num1,double num2);  
+    public double multiplicar(double num1,double num2);  
+    public double dividir(double num1,double num2); 
+}
